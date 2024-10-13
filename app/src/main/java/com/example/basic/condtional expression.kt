3904a3bdcb4cbe3e14c882c865cb8627d77aefb7
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-14 04:45:14 +0700

    add if else

--- app/src/main/java/com/example/basic/condtional expression.kt
+++ app/src/main/java/com/example/basic/condtional expression.kt
@@ -1,2 +1,12 @@
 package com.example.basic
 
+fun ConditionalStatement() {
+    println(" === Conditional Expression === ")
+
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
