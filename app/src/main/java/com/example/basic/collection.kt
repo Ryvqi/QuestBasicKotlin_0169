674306c5bb4a063bf7fd4ec631055159f683cb70
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-14 04:07:30 +0700

    add collection maps: read only

--- app/src/main/java/com/example/basic/collection.kt
+++ app/src/main/java/com/example/basic/collection.kt
@@ -45,3 +45,7 @@ fun ContohMap(){
     println()
     println("=== Map ===")
 
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    
