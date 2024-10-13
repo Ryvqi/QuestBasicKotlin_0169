d3ba81c3265b87edfb3847d7df0d03e0839acab5
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-14 04:27:13 +0700

    instance, access properties and member functions

--- app/src/main/java/com/example/basic/class.kt
+++ app/src/main/java/com/example/basic/class.kt
@@ -4,3 +4,12 @@ class Motor()
 
 class Contact(val id: Int, var email: String)
 
+fun main () {
+    val contact = Contact(1, "mary@gmail.com")
+
+    println(contact.email)
+
+    contact.email = "jane@gmail.com"
+
+    println(contact.email)
+}
