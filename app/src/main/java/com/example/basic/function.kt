2d734157a716b605603d44f019104b5cac4fd963
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-14 04:38:59 +0700

    add function with default parameter value

--- app/src/main/java/com/example/basic/function.kt
+++ app/src/main/java/com/example/basic/function.kt
@@ -16,3 +16,10 @@ fun withNamedArgument(name: String, age: Int) {
     println(" == withNamedArgument == ")
     println("Hello, $name! You are $age years old.")
 }
+
+fun withDefaultParameter(name: String = " Indra", age: Int) {
+    println()
+    println(" == withDefaultParameter == ")
+    println("Hello, $name! You are $age years old.")
+}
+
