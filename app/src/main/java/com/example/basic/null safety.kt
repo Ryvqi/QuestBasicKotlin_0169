ba4ecc84a93e26595e81ce883749f3e4dc620f75
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-14 04:42:53 +0700

    add content for null safety function

--- app/src/main/java/com/example/basic/null safety.kt
+++ app/src/main/java/com/example/basic/null safety.kt
@@ -1,2 +1,28 @@
 package com.example.basic
 
+fun nullSafety() {
+    var neverNull: String = "This can't be null"
+
+    var nullable: String? = "You can keep a null here"
+
+    nullable = null
+
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) // 18
+}
+
+fun main() {
+    nullSafety()
+}
