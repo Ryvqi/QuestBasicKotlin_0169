884856ad32e2fe21de56e76db6155cfbbad698d9
Ryvqi <rifqikhabibi03@gmail.com>
2024-10-14 04:16:36 +0700

    add content for lambda function

--- app/src/main/java/com/example/basic/lambda function.kt
+++ app/src/main/java/com/example/basic/lambda function.kt
@@ -1,2 +1,11 @@
 package com.example.basic
 
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+fun main () {
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+// HELLO
+}
